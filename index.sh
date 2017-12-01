2344472d3edbae8f76c084f41c90d894f54ee72c
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-01 19:00:12 +0800

    update

--- index.sh
+++ index.sh
@@ -8,3 +8,8 @@ npm rm -g pod
 npm i -g https://github.com/zhouzhongyuan/pod.git
 
 pod stop t && pod rm t && pod remote t zhouzhongyuan/tester && pod start t
+
+vi ~/.pm2/logs/pod-web-service-error-1.log
+vi ~/.pm2/logs/pod-web-service-out-1.log
+
+vi ~/.pm2/logs/t-out-2.log
